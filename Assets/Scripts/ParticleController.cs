9979fd0035e4467b0807543a0eb15dc887ab0112
Daniel <idaniel95@me.com>
2016-04-22 11:50:23 +0200

    Donut & ball physics test

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleController : MonoBehaviour {
+
+    [SerializeField]
+    private float sizeToShrinkTo = 0.1f;
+
+    [SerializeField]
+    private float alphaToFadeTo = 0;
+
+    private Shrink shrink;
+
+    private Fade fade;
+
+    void OnEnable()
+    {
+        shrink.FinishedShrinking += DoneShrinking;
+        fade.FinishedFading += DoneFading;
+    }
+
+    void OnDisable()
+    {
+        shrink.FinishedShrinking -= DoneShrinking;
+        fade.FinishedFading -= DoneFading;
+    }
+
+    void Awake() {
+        shrink = GetComponent<Shrink>();
+        fade = GetComponent<Fade>();
+
+        shrink.StartShrinking(sizeToShrinkTo);
+    }
+
+    private void DoneShrinking() {
+        fade.StartFading(alphaToFadeTo);
+    }
+
+    private void DoneFading()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TrailSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject objectToSpawn;
+
+    [SerializeField]
+    private float cooldown = 40;
+
+    private float startCooldown;
+
+    [SerializeField]
+    private float randomCooldown = 0.3f;
+
+    [SerializeField]
+    private float randomPosition = 1;
+
+    void Start() {
+        startCooldown = cooldown;
+        randomCooldown = cooldown * randomCooldown;
+    }
+
+	void FixedUpdate () {
+        cooldown--;
+        if (cooldown < 0) {
+            cooldown = startCooldown + Random.Range(-randomCooldown, randomCooldown);
+            Vector2 randomPos = new Vector2(transform.position.x + Random.Range(-randomPosition, randomPosition), transform.position.y + Random.Range(-randomPosition, randomPosition));
+            Instantiate(objectToSpawn, randomPos, transform.rotation);
+        }
+	}
+}
